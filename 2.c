9826c03678c106067ddb943901d46677577eeb3d
Rustyspotted1 <149468877+Rustyspotted1@users.noreply.github.com>
2023-11-05 12:13:02 +0900

    "2.c" added and "myProject.h" modified
    2.c 추가 및 myProject.h에 함수 추가

---
+++ 2.c
@@ -0,0 +1,6 @@
+#include "myProject.h"
+int printMyName2(void)
+{
+    printf("2. my name is Sangwoon\n");
+    return 1;
+}
--- myProject.h
+++ myProject.h
@@ -1,6 +1,8 @@
 #ifndef MY_PROJECT_H
 #define MY_PROJECT_H
 
+int printMyName2(void);
 int printMyName4(void);
+
 
 #endif // MY_PROJECT_H
