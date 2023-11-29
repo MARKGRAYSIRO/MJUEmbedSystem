a066d42d6b82705c81f943547d8a3d99e07e06d3
ThingVirus <141991605+AhnMyeongGyun123@users.noreply.github.com>
2023-11-29 13:01:58 +0900

    Just little bit changed

--- hw_2_LED_Butten_Buzzer/led.c
+++ hw_2_LED_Butten_Buzzer/led.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -6,29 +7,27 @@
 static unsigned int ledValue = 0;
 static int fd = 0;
 
+int ledOnOff (int ledNum, int onoff){
+    int i = 1;
+    i = i << ledNum;
+    ledValue = ledValue & (~i);
+    if (onoff != 0) ledValue |= i;
+    wirte (fd, &ledValue, 4);
+}
+
 int ledLibInit(void){
     fd=open("/dev/periled",O_WRONLY);
-    ledValue = 0;
     if(fd < 0){
-        printf("ledLibInit failed");
-        return -1;
+        printf("driver (//dev//cnled) open error.\n");
+        return 1;
     }
-    return 0;
+    ledValue = 0;
 }
 
 int ledLibExit(void){
     ledValue = 0;
     ledOnOff(0,0);
     close(fd);
-}
-
-int ledOnOff (int ledNum, int onoff)
-{
-    int i=1;
-    i = i<<ledNum; 
-    ledValue = ledValue& (~i);
-    if (onoff != 0) ledValue |=i;
-    write (fd, &ledValue,4);
 }
 
 int ledstatus (void){
