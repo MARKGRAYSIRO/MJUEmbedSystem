b9b2c42c855e2c8f0796e24f63a0e9b3b6431fe3
ThingVirus <141991605+AhnMyeongGyun123@users.noreply.github.com>
2023-12-05 22:26:30 +0900

    I added many thing hhhh

--- hw_2_LED_Button_Buzzer_/buzzertest/buzzer.c
+++ hw_2_LED_Button_Buzzer_/buzzertest/buzzer.c
@@ -6,8 +6,7 @@
 #include <dirent.h>
 
 #define MAX_SCALE_STEP          8
-#define BUZZER_BASE_SYS_PATH    "/sys/bus/platform/devices/"
-#define BUZZER_FILENAME         "peribuzzzer"
+#define BUZZER_BASE_SYS_PATH    "/sys/bus/platform/devices/peribuzzer."
 #define BUZZER_ENABLE_NAME      "enable"
 #define BUZZER_FREQUENCY_NAME   "frequency"
 char gBuzzerBaseSysDir[128];
