1d72e090490c385b4ab4b9997e8c2cb7f50e238e
Rustyspotted1 <149468877+Rustyspotted1@users.noreply.github.com>
2023-12-19 21:41:14 +0900

    빗자루게임 각도 측정
    custom_atan함수를 적용한 빗자루게임 코드

--- sensor_value/broomgame.c
+++ sensor_value/broomgame.c
@@ -2,13 +2,9 @@
 #include <unistd.h>
 #include "accelMagGyro.h"
 
-void readGyro() {
-    // Array to store gyroscope data (x, y, z)
-    int gyro[3];
-
+void readGyro(int* gyro) {
     if (getAccel(gyro) != 0) {
         printf("Failed to get gyroscope values.\n");
-        return -1;
     }
 }
 
@@ -18,13 +14,14 @@ int main() {
     int totalTime = 10;         //측정 시간 지정
     int delay = totalTime * 1000000 / numIterations;
 
+    int gyro[3];
     int area = 0;
     int broomscore = 0;
 
     // Loop through each set of coordinates
     for (int i = 1; i < numIterations; ++i) {
 
-        readGyro();
+        readGyro(gyro);
 
         // Extract x and y values from gyroscope data
         int x = gyro[0];
@@ -33,7 +30,7 @@ int main() {
         // Define the position on the Cartesian system
         // Divide area by 3 x 3
         //각 if문 안에 빗자루 사진 코드 삽입할 것
-        
+
         if (x > -1000 && x < 1000 && y > -1000 && y < 1000) {
             area = 0;
         }
@@ -41,11 +38,11 @@ int main() {
             area = 1;
             ++broomscore;
         }
-        else if (x < 1000 && y > -1000 && y < 1000) {
+        else if (x > 1000 && y > -1000 && y < 1000) {
             area = 2;
             ++broomscore;
         }
-        else if (x < 1000 && y < -1000) {
+        else if (x > 1000 && y < -1000) {
             area = 3;
             ++broomscore;
         }
@@ -69,12 +66,12 @@ int main() {
             area = 8;
             ++broomscore;
         }
-        printf("Area : %d", area);
+        printf("Area : %d\n", area);
         usleep(delay);
     }
 
     //Game ended. Printing result
-    printf("\n\nScore : %d", broomscore);
+    printf("\n\nScore : %d\n", broomscore);
+    return 0;
 
 }
-
